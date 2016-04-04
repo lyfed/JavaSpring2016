009350e21ae9c19a73fe02728cd720f2b760c359
Liubov Fedorovich <luba.f89@gmail.com>
2016-04-04 19:37:51 +0300

    Homework02: fixed namings

--- Homeworks/src/lesson160331/Salary.java
+++ Homeworks/src/lesson160331/Salary.java
@@ -2,15 +2,15 @@ package lesson160331;
 
 public class Salary {
 
-	final static float tax = 0.87f;
+	final static float TAX = 0.87f;
 	
 	public static void main(String[] args) {
 		
 		int salary = 20_000;
-		float salaryWithoutTax = salary * tax;
+		float salaryWithoutTax = salary * TAX;
 		
 		System.out.println("salary = " + salary);
-		System.out.format("salary without tax = " + (int)salaryWithoutTax);
+		System.out.println("salary without tax = " + (int)salaryWithoutTax);
 
 	}
 
